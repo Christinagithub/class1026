47703810797cde502c1f051cb450a2b3a92745c8
Christinagithub <2605839246@qq.com>
2018-10-26 20:30:13 +0800

    Initial Commit

---
+++ class1026Tests/class1026Tests.swift
@@ -0,0 +1,34 @@
+//
+//  class1026Tests.swift
+//  class1026Tests
+//
+//  Created by s20171105407 on 2018/10/26.
+//  Copyright © 2018 s20171105407. All rights reserved.
+//
+
+import XCTest
+@testable import class1026
+
+class class1026Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
